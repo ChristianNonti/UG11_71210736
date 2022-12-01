47f5bf90a14aded37bb77472ea14346338182127
ChristianNonti <93360281+ChristianNonti@users.noreply.github.com>
2022-12-01 16:23:18 +0700

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,17 @@
+print("Halo Selamat Datang di bioskop!")
+print("Dimnakah kamu ingin menonton?")
+
+def tempat():
+    print("1) XX1 Empire\n2) XXI Emplaz\n3) XXI JCM")
+    if pilihan:
+        pilihan = pilihan >= 3
+    
+    else:
+        print("pilihan tidak tesedia.")
+    print("Mau nonton film apa nih ? ada film :\n 1. Frozen \n 2. Keramat \n 3. KKN di desa penari")
+    
+    
+        
+pilihan = int(input("Masukkan Pilihan :"))
+
+tempat()
---
+++ 2.py
@@ -0,0 +1,8 @@
+def cetak_huruf():
+    if a % 2 == 0:
+        print("")
+    
+
+a = input("Masukkan Kata :")
+
+cetak_huruf()
